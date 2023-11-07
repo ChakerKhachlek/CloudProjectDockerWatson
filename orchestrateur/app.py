e0b26ccb9065946e8ef0ab609b310e144fcbb939
Chaker Khachlek <chaker-2@hotmail.com>
2023-11-07 13:48:48 +0100

    Update app.py

--- orchestrateur/app.py
+++ orchestrateur/app.py
@@ -5,12 +5,12 @@ import redis
 import json
 from flask_cors import CORS
 app = Flask(__name__)
-
+CORS(app) 
 # URL of the original app that connects to Watson Assistant
-ORIGINAL_APP_URL = "http://localhost:5000"  # Replace with the actual host of the original app
+ORIGINAL_APP_URL = "http://api:5000"  # Replace with the actual host of the original app
 
 # Redis connection
-redis_client = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)  # Change 'localhost' to 'redis'
+redis_client = redis.StrictRedis(host='redis', port=6379, db=0, decode_responses=True)  # Change 'localhost' to 'redis'
 @app.route('/message', methods=['POST'])
 def handle_message():
     data = request.get_json()
@@ -37,6 +37,6 @@ def handle_message():
             return jsonify({'error': 'Original app error'})
     except Exception as e:
         return jsonify({'error': str(e)})
-CORS(app)
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
